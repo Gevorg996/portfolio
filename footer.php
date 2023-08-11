71b8e1b61c74662e37a180ce9af6d03a3c7606ff
Gevorg996 <142011251+Gevorg996@users.noreply.github.com>
2023-08-11 14:59:15 +0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+<div class="footer">
+  <h1>Aqua Store</h1>
+  <div class="copyright ml-auto mr-auto">
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This shop is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#" target="_blank">Students</a>
+  </div>
+  <ul>
+    <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+    <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+  </ul>
+</div>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script> -->
+<script src="js/js.js"></script>
+</body>
+</html>
